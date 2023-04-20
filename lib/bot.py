457cc8e02a350ba957d1c6121febbcf7c42421bf
Gavin Guo <gavin.guo@canonical.com>
2023-04-20 15:52:16 +0800

    LLM: Support OpenAI/Llamacpp models
    Support two models in the frameworks. Currently, we can configure the
    adapting models inside the config.yaml. The OpenAI API key also needs to
    apply by yourself. For the model_name used by the OpenAI, you can refer to
    the python file under Langchain project: langchain/callbacks/openai_info.py:
    
        model_cost_mapping = {
            "gpt-4": 0.03,
            "gpt-4-0314": 0.03,
            "gpt-4-completion": 0.06,
            "gpt-4-0314-completion": 0.06,
            "gpt-4-32k": 0.06,
            "gpt-4-32k-0314": 0.06,
            "gpt-4-32k-completion": 0.12,
            "gpt-4-32k-0314-completion": 0.12,
            "gpt-3.5-turbo": 0.002,
            "gpt-3.5-turbo-0301": 0.002,
            "text-ada-001": 0.0004,
            "ada": 0.0004,
            "text-babbage-001": 0.0005,
            "babbage": 0.0005,
            "text-curie-001": 0.002,
            "curie": 0.002,
            "text-davinci-003": 0.02,
            "text-davinci-002": 0.02,
            "code-davinci-002": 0.02,
        }
    
    There are corresponding prices for your reference.
    
    Signed-off-by: Gavin Guo <gavin.guo@canonical.com>

--- lib/bot.py
+++ lib/bot.py
@@ -1,5 +1,6 @@
 import time
 import threading
+import yaml
 from .llm import LLM
 from .prompt_generator import PromptGenerator
 from .qa_chain import QAChain
@@ -8,12 +9,23 @@ from .vectorstore import VectorStore
 stop_event = threading.Event()
 
 class Bot:
-    def __init__(self, config):
-        data_dir = config['data_dir']
-        model_path = config['model_path']
-        qa_chain_type = config['qa_chain_type'] if 'qa_chain_type' in config else 'stuff'
-        max_res_num = config['max_res_num'] if 'max_res_num' in config else 3
-        self.llm = LLM(model_path)
+
+    def __init__(self, CONFIG_PATH):
+        with open(CONFIG_PATH, 'r') as f:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+
+        default_llm = config.get('default_llm')
+        if default_llm is None:
+            raise ValueError("default_llm is necessary; however, the config is [{}]".format(config))
+
+        data_dir = config.get('data_dir')
+        if data_dir is None:
+            raise ValueError("data_dir is necessary; however, the config is [{}]".format(config))
+
+        qa_chain_type = config.get('qa_chain_type', 'stuff')
+        max_res_num = config.get('max_res_num', 3)
+
+        self.llm = LLM(config)
         self.prompt_generator = PromptGenerator()
         self.vector_store = VectorStore(data_dir, self.llm)
         self.qa_chain = QAChain(qa_chain_type, max_res_num, self.llm, self.vector_store, self.prompt_generator)
--- lib/llm.py
+++ lib/llm.py
@@ -1,7 +1,33 @@
 from langchain.embeddings.llamacpp import LlamaCppEmbeddings
-from langchain.llms import LlamaCpp
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import LlamaCpp, OpenAI
+
 
 class LLM:
-    def __init__(self, model_path):
-        self.llm = LlamaCpp(model_path=model_path, n_ctx=2048)
-        self.embedding = LlamaCppEmbeddings(model_path=model_path, n_ctx=2048)
+
+    def __init__(self, config):
+        # get default language model (LLM) config
+        default_llm = config.get('default_llm')
+        llm_config = config.get(default_llm)
+
+        # get model path, model name, and OpenAI API key
+        model_path = llm_config.get('model_path')
+        model_name = llm_config.get('model_name')
+        openai_api_key = llm_config.get('openai_api_key')
+
+        # create LLM and embedding based on LLM config
+        if default_llm == 'llamacpp':
+            if not model_path:
+                raise ValueError("Missing model_path in config")
+
+            self.llm = LlamaCpp(model_path=model_path, n_ctx=2048)
+            self.embedding = LlamaCppEmbeddings(model_path=model_path, n_ctx=2048)
+
+        elif default_llm == 'openai':
+            if not openai_api_key:
+                raise ValueError("Missing openai_api_key in config")
+
+            self.llm = OpenAI(openai_api_key=openai_api_key, model_name=model_name)
+            self.embedding = OpenAIEmbeddings(openai_api_key=openai_api_key)
+        else:
+            raise ValueError("Unknown LLM type: {}".format(default_llm))
