791f405a97128d49e3f21984691b042dd8714ae3
Gerald Yang <gerald.yang@canonical.com>
2023-04-25 07:59:43 +0000

    Modify pull-git.py to pull git commit from networking subsystem
    Signed-off-by: Gerald Yang <gerald.yang@canonical.com>

--- lib/pull-git.py
+++ lib/pull-git.py
@@ -5,7 +5,8 @@ origdir = os.getcwd()
 gitdir = origdir + '/git-data'
 os.chdir("linux")
 
-cmd = os.popen('git log --since="2023-01-01" --grep="^mm:" --grep="^mm/.*:" --oneline')
+#cmd = os.popen('git log --since="2023-01-01" --grep="^mm:" --grep="^mm/.*:" --oneline')
+cmd = os.popen('git log --since="2023-01-01" --grep="^net:" --grep="^net/.*:" --oneline | grep -v " Merge branch "')
 output = cmd.read()
 output_list = output.splitlines()
 
@@ -23,5 +24,25 @@ for commit in output_list:
     with open(gitdir + '/' + commit_id + '.data', 'w') as cfile:
         cmd = os.popen('git show ' + commit.split()[0])
         commit_log = cmd.read()
-        cfile.write(commit_log.split('diff --git')[0])
+        commit_log = commit_log.split('diff --git')[0]
+        commit_log_post = commit_log.split('\n')
+        commit_log_post.insert(3, 'content:')
+        for i, line in enumerate(commit_log_post):
+            if i < 4:
+                cfile.write(line + '\n')
+            if i >= 4 and i < 50:
+                cfile.write(line)
 
+        """
+        people = 0
+        for i, line in enumerate(commit_log_post):
+            if 'Fixes: ' in line:
+                people = i
+                break
+            if 'Signed-off-by:' in line:
+                people = i
+                break
+        commit_log_post.insert(people, 'people:')
+        for i, line in enumerate(commit_log_post):
+            cfile.write(line + '\n')
+        """
