4acb6e82078f05563b40b87f41275868e3b5251a
Chengen Du <chengen.du@canonical.com>
2024-04-11 15:14:34 +0800

    Implement remote model

---
+++ remote_model/remote_model.py
@@ -0,0 +1,39 @@
+from kserve import Model, ModelServer
+from sentence_transformers import SentenceTransformer
+from transformers import LlamaForCausalLM, LlamaTokenizer
+from typing import Dict
+
+
+class RemoteLlamaModel(Model):
+    def __init__(self):
+        self.name = 'llama-model'
+        super().__init__(self.name)
+        self.model_dir = './models/Llama-2-7b-chat-hf'
+        self.load()
+
+    def load(self):
+        self.tokenizer = LlamaTokenizer.from_pretrained(self.model_dir)
+        self.inference_model = LlamaForCausalLM.from_pretrained(self.model_dir)
+        self.embeddings_model = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
+
+    async def predict(self, payload: Dict, headers: Dict[str, str] = None) -> Dict:
+        texts = payload['texts']
+        outputs = []
+        match payload['type']:
+            case 'inference':
+                for text in texts:
+                    input_ids = self.tokenizer.encode(text, return_tensors="pt")
+                    generated_ids = self.inference_model.generate(input_ids, temperature=1, do_sample=False)
+                    output = self.tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+                    outputs.append(output)
+            case 'embeddings':
+                for text in texts:
+                    embeddings = self.embeddings_model.encode(text)
+                    outputs.append(embeddings.tolist())
+
+        return {'outputs': outputs}
+
+if __name__ == "__main__":
+    model = RemoteLlamaModel()
+    model_server = ModelServer(http_port=8080, workers=1)
+    model_server.start([model])
