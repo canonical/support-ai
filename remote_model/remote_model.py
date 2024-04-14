7ecb185082e05b2328eedcd22c4b7024816d7680
Chengen Du <chengen.du@canonical.com>
2024-04-14 13:46:20 +0800

    Refine inference output in remote server

--- remote_model/remote_model.py
+++ remote_model/remote_model.py
@@ -24,7 +24,7 @@ class RemoteLlamaModel(Model):
                 for text in texts:
                     input_ids = self.tokenizer.encode(text, return_tensors="pt")
                     generated_ids = self.inference_model.generate(input_ids, temperature=1, do_sample=False)
-                    output = self.tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+                    output = self.tokenizer.batch_decode(generated_ids[:, input_ids.shape[1]:], skip_special_tokens=True)[0]
                     outputs.append(output)
             case 'embeddings':
                 for text in texts:
@@ -32,6 +32,7 @@ class RemoteLlamaModel(Model):
                     outputs.append(embeddings.tolist())
 
         return {'outputs': outputs}
+
 
 if __name__ == "__main__":
     model = RemoteLlamaModel()
